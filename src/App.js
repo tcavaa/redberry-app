f5636e1432930cd818e689be60c6a1a1d7826b74
torniketsava <51153900+torniketsava@users.noreply.github.com>
2022-09-05 17:44:10 +0400

    Form & list page created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './components/stylesheets/app.css';
-import { Outlet, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 
 const App = () => {
--- src/components/list.js
+++ src/components/list.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
+import './stylesheets/list.css';
 
 const List = () => {
     const helo  = '22'
     return (
-        <div>
-            List
+        <div className='list'>
+            <NavLink to="/" className='backIcon'></NavLink>
+            <h1>ჩანაწერების სია</h1>
+            <div className='listContainer'>
+                <div className='listItem'>
+                    <a className='image' href='/laptopinfo'><img /></a>
+                    <div>
+                        <h2>ირინე ჩანქელიანი</h2>
+                        <p>Pentium II</p>
+                        <NavLink to="/laptopinfo" className='seemore'>მეტის ნახვა</NavLink>
+                    </div>
+                </div>
+                <div className='listItem'>
+                    <a className='image' href='/laptopinfo'><img /></a>
+                    <div>
+                        <h2>ირინე ჩანქელიანი</h2>
+                        <p>Pentium II</p>
+                        <NavLink to="/laptopinfo" className='seemore'>მეტის ნახვა</NavLink>
+                    </div>
+                </div>
+                <div className='listItem'>
+                    <a className='image' href='/laptopinfo'><img /></a>
+                    <div>
+                        <h2>ირინე ჩანქელიანი</h2>
+                        <p>Pentium II</p>
+                        <NavLink to="/laptopinfo" className='seemore'>მეტის ნახვა</NavLink>
+                    </div>
+                </div>
+                <div className='listItem'>
+                    <a className='image' href='/laptopinfo'><img /></a>
+                    <div>
+                        <h2>ირინე ჩანქელიანი</h2>
+                        <p>Pentium II</p>
+                        <NavLink to="/laptopinfo" className='seemore'>მეტის ნახვა</NavLink>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
