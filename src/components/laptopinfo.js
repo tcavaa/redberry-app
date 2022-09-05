4c1ef6c47a9a44bc1eee93feb96a3de0e3bdfa65
torniketsava <51153900+torniketsava@users.noreply.github.com>
2022-09-05 23:36:10 +0400

    Validations & apis

--- src/components/laptopinfo.js
+++ src/components/laptopinfo.js
@@ -1,8 +1,54 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
+import API from '../api/Api';
+import {useState, useEffect} from 'react';
 import './stylesheets/laptopinfo.css';
 
 const LaptopInfo = () => {
+
+    const [item, setItem] = useState({
+        "user": {
+            "name": "გელა",
+            "surname": "გელაშვილი",
+            "team_id": 1,
+            "position_id": 1,
+            "email": "gela.gelashvili@redberry.ge",
+            "phone_number": "+995555555555"
+        },
+        "laptop": {
+            "name": "HP",
+            "image": "/storage/images/4nBeQnWa21ExiBZVInqlNx0nUp2UIxXRtNTzUxet.png",
+            "brand_id": 1,
+            "cpu": {
+                "name": "Intel Core i3",
+                "cores": 64,
+                "threads": 128
+            },
+            "ram": 34,
+            "hard_drive_type": "HDD",
+            "state": "new",
+            "purchase_date": "10-10-2003",
+            "price": 1600
+        }
+    });
+
+    const listLaptop = async () => {
+        try {
+          const alldata = await API.getlaptop(window.location.pathname.split('/')[2])
+          setItem(alldata)
+        }
+        catch(error) {
+          console.log("Please, try again later");
+          throw error;
+        }
+      }
+    
+      useEffect(() => {
+        listLaptop();
+      }, []);
+
+      console.log(item);
+
     return (
         <div className='mainCont'>
             <NavLink to="/" className='backIcon'></NavLink>
@@ -13,23 +59,23 @@ const LaptopInfo = () => {
                     <table>
                         <tr>
                             <th>სახელი:</th>
-                            <td>აკაკი წერეთელი</td>
+                            <td>{item.user.name} {item.user.surname}</td>
                         </tr>
                         <tr>
                             <th>თიმი:</th>
-                            <td>დიზაინერები</td>
+                            <td>{item.user.team_id}</td>
                         </tr>
                         <tr>
                             <th>პოზიცია:</th>
-                            <td>ილუსტრატორი</td>
+                            <td>{item.user.position_id}</td>
                         </tr>
                         <tr>
                             <th>მეილი:</th>
-                            <td>ako@redberry.ge</td>
+                            <td>{item.user.email}</td>
                         </tr>
                         <tr>
                             <th>ტელ. ნომერი:</th>
-                            <td>+995 583 38 22 33</td>
+                            <td>{item.user.phone_number}</td>
                         </tr>
                     </table>
                 </div>
@@ -37,33 +83,33 @@ const LaptopInfo = () => {
                 <table>
                         <tr>
                             <th>ლეპტოპის სახელი:</th>
-                            <td>Razor Bla Bla</td>
+                            <td>{item.laptop.name}</td>
                         </tr>
                         <tr>
                             <th>ლეპტოპის ბრენდი:</th>
-                            <td>HP</td>
+                            <td>{item.laptop.brand_id}</td>
                         </tr>
                         <tr>
                             <th>RAM:</th>
-                            <td>16</td>
+                            <td>{item.laptop.ram}</td>
                         </tr>
                         <tr>
                             <th>მეხსიერების ტიპი:</th>
-                            <td>SSD</td>
+                            <td>{item.laptop.hard_drive_type}</td>
                         </tr>
                     </table>
                     <table>
                         <tr>
                             <th>CPU:</th>
-                            <td>intel 5</td>
+                            <td>{item.laptop.cpu.name}</td>
                         </tr>
                         <tr>
                             <th>CPU-ს ბირთვი:</th>
-                            <td>13</td>
+                            <td>{item.laptop.cpu.cores}</td>
                         </tr>
                         <tr>
                             <th>CPU-ს ნაკადი:</th>
-                            <td>67</td>
+                            <td>{item.laptop.cpu.threads}</td>
                         </tr>
                     </table>
                 </div>
@@ -71,17 +117,17 @@ const LaptopInfo = () => {
                 <table>
                         <tr>
                             <th>ლეპტოპის მდგომარეობა:</th>
-                            <td>მეორადი</td>
+                            <td>{item.laptop.state}</td>
                         </tr>
                         <tr>
                             <th>ლეპტოპის ფასი:</th>
-                            <td>1500</td>
+                            <td>{item.laptop.price}</td>
                         </tr>
                     </table>
                     <table>
                         <tr>
                             <th>შეძენის რიცხვი:</th>
-                            <td>12 / 06 / 2130</td>
+                            <td>{item.laptop.purchase_date}</td>
                         </tr>
                     </table>
                 </div>
